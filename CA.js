8aaf0b403286a983e217612e5db1296ae3974db2
Paul Abrams <john.paul.abrams@gmail.com>
2016-01-19 17:14:30 -0500

    CA.js:  Make it more OO
    Attempt to make this object oriented

--- CA.js
+++ CA.js
@@ -1,103 +1,115 @@
+"use strict";
 
-// page-level variables
-var cellSizeX = 10;
-var cellSizeY = 10;
-var mouseIsDown = false;
+// Create global namespace object
+var CA = CA || {};
 
+// Function to help with Adding event listener to appropriate object
+// Got this idea from http://stackoverflow.com/questions/15356936/object-oriented-javascript-event-handling
+CA.bind = function(scope, fn){
+  return function() {
+    return fn.apply(scope, arguments);
+  }
+}
+
+// Create variable under CA scope to indicate whether mouse is down.
+CA.mouseIsDown = false;
+
+// mouseUp function
+CA.mouseUp = function(event){
+  CA.mouseIsDown = false;
+};
+
+  
+// todo: add grid cells
+// // GridCell constructor
+// var GridCell = function(){
+  // this.status = 0; // by default, a cell is empty.
+// };
+
+// Grid constructor
+var Grid = function(canvasId, width, height, cellSizeX, cellSizeY){
+
+  this.cellSizeX = cellSizeX;
+  this.cellSizeY = cellSizeY;
+  
+  this.canvas = document.getElementById(canvasId);
+  this.canvas.width = width;
+  this.canvas.height = height;
+  
+  this.context = this.canvas.getContext("2d");
+  
+  this.drawGrid = function(){
+    // draw vertical lines
+    for (var i=0; i<this.canvas.width / this.cellSizeX; i++){
+      this.context.beginPath();
+      this.context.moveTo(i*this.cellSizeX - 0.5,0);
+      this.context.lineTo(i*this.cellSizeX -0.5, this.canvas.height);
+      this.context.stroke();
+    }
+
+    // draw horizontal lines
+    for (var j=0; j<this.canvas.height / this.cellSizeY; j++){
+      this.context.beginPath();
+      this.context.moveTo(0,j*this.cellSizeY - 0.5);
+      this.context.lineTo(this.canvas.width, j*this.cellSizeY - 0.5);
+      this.context.stroke();
+    }
+  };
+  
+  this.fillCell = function(x, y, color){  
+    // set color to black if null
+    if ((color === null) || (color === 'undefined')){
+      color = "#000000";
+    }
+    
+    this.context.fillStyle = color;
+
+    var xstart = Math.floor(x / this.cellSizeX) * this.cellSizeX;
+    var ystart = Math.floor(y / this.cellSizeY) * this.cellSizeY;
+    this.context.fillRect(xstart, ystart, this.cellSizeX, this.cellSizeY);
+  };
+  
+  
+  // needed for when user clicks without moving mouse
+  this.click = function(event){
+    var x = event.offsetX;
+    var y = event.offsetY;
+    this.fillCell(x,y);
+  };
+
+  this.mouseMove = function(event){
+    var x = event.offsetX;
+    var y = event.offsetY;
+    
+    // document.getElementById("mouseCoords").innerHTML = x + ',' + y;
+    
+    // if mouse is down, fill cells
+    if (CA.mouseIsDown){
+      this.fillCell(x, y);
+    }
+  };
+  
+  this.mouseDown = function(event){
+    CA.mouseIsDown = true;
+  };
+  
+  // add click event listener to canvas element
+  this.canvas.addEventListener('click', CA.bind(this, this.click), false);
+  
+  // add mouseDown event listener to canvas element
+  this.canvas.addEventListener('mousedown', CA.bind(this, this.mouseDown), false);
+  
+  // add mouseMove event listener to canvas element
+  this.canvas.addEventListener('mousemove', CA.bind(this, this.mouseMove), false);
+  
+};
 
 
 function loadPage(){
-    
-  drawGrid();
+  // add mouseup event listener to document
+  document.addEventListener('mouseup', CA.mouseUp, false);
   
-  // add click event listener to canvas element
-  document.getElementById("myCanvas").addEventListener('click', canvasClick, false);
-  
-  
-  // add mouseDown event listener to canvas element
-  document.getElementById("myCanvas").addEventListener('mousedown', canvasDown, false);
-  
-  // add mouseup event listener
-  document.addEventListener('mouseup', mouseUp, false);
-  
-  // add mouseMove event listener to canvas element
-  document.getElementById("myCanvas").addEventListener('mousemove', canvasMove, false);
-  
-};
-
-function getCtx(){
-  return document.getElementById("myCanvas").getContext("2d");
-};
-
-function drawGrid(){
-  var ctx = getCtx();
-
-  // draw vertical lines
-  for (i=0; i<ctx.canvas.width / cellSizeX; i++){
-    ctx.beginPath();
-    ctx.moveTo(i*cellSizeX - 0.5,0);
-    ctx.lineTo(i*cellSizeX -0.5, ctx.canvas.height);
-    ctx.stroke();
-  }
-
-  // draw horizontal lines
-  for (j=0; j<ctx.canvas.height / cellSizeY; j++){
-    ctx.beginPath();
-    ctx.moveTo(0,j*cellSizeY - 0.5);
-    ctx.lineTo(ctx.canvas.width, j*cellSizeY - 0.5);
-    ctx.stroke();
-  }
-};
-
-// fill the cell located at the coordinates x, y
-function fillCell(x, y, color){
-  var ctx = getCtx();
-     
-  // set color to black if null
-  if ((color === null) || (color === 'undefined')){
-    color = "#000000";
-  }
-  
-  ctx.fillStyle = color;
-  
-  var xstart = Math.floor(x / cellSizeX) * cellSizeX;
-  var ystart = Math.floor(y / cellSizeY) * cellSizeY;
-  ctx.fillRect(xstart, ystart, cellSizeX, cellSizeY);
-  
-};
-
-// this is needed for when user clicks without moving mouse
-function canvasClick(event){
-  var ctx=getCtx();
-  var x = event.offsetX;
-  var y = event.offsetY;
-  
-  
-  fillCell(x,y);
-};
-
-function canvasMove(event){
-  var ctx=getCtx();
-  
-  var x = event.offsetX;
-  var y = event.offsetY;
-  
-  document.getElementById("mouseCoords").innerHTML = x + ',' + y;
-  
-  // if mouse is down, fill cells
-  if (mouseIsDown){
-    fillCell(x, y);
-  }
-  
-};
-
-
-function canvasDown(event){
-  mouseIsDown = true;
-};
-
-
-function mouseUp(event){
-  mouseIsDown = false;
+  CA.grid = new Grid("myCanvas", 800, 800, 10, 10);
+  CA.grid.drawGrid();
 };
 
